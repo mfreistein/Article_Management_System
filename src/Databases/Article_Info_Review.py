6dcf06dee40020d41f2cbf25a8fde1e7e20bbd57
Manuel Freistein <manuelfreistein@Manuels-MBP.fritz.box>
2022-01-27 14:07:20 +0100

    fst

--- src/Databases/Article_Info_Review.py
+++ src/Databases/Article_Info_Review.py
@@ -1,6 +1,7 @@
 import mysql.connector as mysql
 
-ams_db = mysql.connect(host="localhost", user="root", password="", database="AMS")
+#ams_db = mysql.connect(host="localhost", user="root", password="", database="AMS")
+ams_db = mysql.connect(host="%manuelfreistein", user="ManuelFreistein", password="AMS", database="AMS")
 command_handler = ams_db.cursor(buffered=True)
 
 def get_all_articles_in_review() -> list:
