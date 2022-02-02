cc366b038c7e16074775af860b7e28735fe4b560
Manuel Freistein <manuel-freistein@web.de>
2022-02-02 14:36:35 +0100

    test

--- src/Conception/View_tests.py
+++ src/Conception/View_tests.py
@@ -3,5 +3,6 @@ from unittest import TestCase
 
 class TestView(TestCase):
     def print_invalid_input(self):
+        #HALLLLO
         from View import View
         self.assert_stdout("This is not a valid input. Please try again")
